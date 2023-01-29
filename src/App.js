51511c25f809dee351f1a44a863f1ebeca415ee8
idowuoluwasanmi <idowuoluwasanmi@gmail.com>
2023-01-29 22:37:24 +0000

    components and css

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import CryptoList from './CryptoList';
 const App = () => {
   const [coinsData, setCoinsData] = useState([]);
 
-  useEffect(async () => {
-    
-    const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-    
-    setCoinsData(response.data);
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      setCoinsData(response.data);
+    }
+    loadData();
   }, []);
 
-  
   return (
     <div className='app'>
       <h1>Crypto Gallery</h1>
       <CryptoList coinsData={coinsData} />
-      {console.log(coinsData)}
     </div>
   )
 }
--- src/CryptoList.js
+++ src/CryptoList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import CryptoCard from "./CryptoCard";
 
-const CryptoList = () => {
+const CryptoList = ({ coinsData }) => {
 
     return(
-        <div>
-            fg
+        <div className="crypto_list">
+            {
+                coinsData?.map((coin, index) => {
+                    return (
+                        <CryptoCard 
+                            key={index}
+                            name={coin.name}
+                            image={coin.image}
+                            price={coin.current_price}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
